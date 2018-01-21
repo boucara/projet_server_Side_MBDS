654e7fd9c5cc60477baba64ca5f8a12f04ae09d1
boucara <traorebelco@gmail.com>
2018-01-21 21:28:21 +0100

    service angular pour les requetes video

--- angular_client/client-video/src/app/app.component.ts
+++ angular_client/client-video/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+  constructor(private http: HttpClient){
+    
+  }
   title = 'app';
 }
