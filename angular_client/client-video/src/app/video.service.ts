b22826212457b52a06856161339c16e262fb019d
boucara <traorebelco@gmail.com>
2018-01-26 03:30:52 +0100

    resolution conflits

--- angular_client/client-video/src/app/video.service.ts
+++ angular_client/client-video/src/app/video.service.ts
@@ -1,22 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-<<<<<<< HEAD
+
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
-=======
->>>>>>> 2ba871741701de9778d9041b14fc92f6e75f7028
 
 @Injectable()
 export class VideoService {
 
-<<<<<<< HEAD
   constructor(private http:Http) {
-     
-=======
-  constructor(private http:HttpClient) {
-
->>>>>>> 2ba871741701de9778d9041b14fc92f6e75f7028
-   }
+    
+  }
    private url ='/api/videos'
    getVideos(){
     return this.http.get(this.url).map((response: Response) => response.json());
