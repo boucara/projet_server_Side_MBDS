2fccce9e02f59fedb628314f21c09f579c51ee39
boucara <traorebelco@gmail.com>
2018-01-26 17:54:15 +0100

    ajut component ajout video et reorganisation client

---
+++ angular_client/client-video/src/app/accueil/accueil.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { VideoService } from '../video.service';
+import {Http, Response} from '@angular/http';
+import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-accueil',
+  templateUrl: './accueil.component.html',
+  styleUrls: ['./accueil.component.css']
+})
+export class AccueilComponent implements OnInit {
+  videos:String[];
+  data:object;
+  tabdata = new Array();
+  constructor(private http: Http , private videoService:VideoService, private sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+    this.videoService.getVideos().subscribe(response =>{
+      this.videos=response.data ;
+
+      console.log(this.videos);
+      });
+
+  }
+  serialiseURL(url:string): SafeUrl{
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+}
--- angular_client/client-video/src/app/app.module.ts
+++ angular_client/client-video/src/app/app.module.ts
@@ -5,16 +5,28 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {VideoService} from'./video.service'
 import { HttpModule } from '@angular/http';
+import { AccueilComponent } from './accueil/accueil.component';
+import { AddvideoComponent } from './addvideo/addvideo.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'addvideo', component: AddvideoComponent },
+ 
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    AddvideoComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [VideoService ],
   bootstrap: [AppComponent]
